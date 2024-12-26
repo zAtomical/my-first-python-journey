8d3e9ee06f87644e0469e8049ed79023b2a2bcfd
zAtomic <atharvsriv99@gmail.com>
2024-12-26 13:53:20 +0530

    Refactored The QnAs
    Moved questions and answers into a seperate file

--- QuizTimeV2/QuizTime.py
+++ QuizTimeV2/QuizTime.py
@@ -1,23 +1,6 @@
 #quiz game
 from CheckIntent import start_quiz
-
-questions = (
-    "Is Australia Real?",
-    "Am I cool?",
-    "AmongUs or Fortnite",
-    "Yes or No?",
-    "What is the capital of France?",
-    "By the way if Australia is real, what is the capital of Australia?"
-)
-
-answers = (
-    "yes",
-    "yes",
-    "amongus",
-    "no",
-    "paris",
-    "canberra"
-)
+from QuizTuples import questions, answers
 
 while True:
     score = 0
---
+++ QuizTimeV2/QuizTuples.py
@@ -0,0 +1,17 @@
+questions = (
+    "Is Australia Real?",
+    "Am I cool?",
+    "AmongUs or Fortnite",
+    "Yes or No?",
+    "What is the capital of France?",
+    "By the way if Australia is real, what is the capital of Australia?"
+)
+
+answers = (
+    "yes",
+    "yes",
+    "amongus",
+    "no",
+    "paris",
+    "canberra"
+)
