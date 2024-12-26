c61c6f93b4e1a59c3d2fd2ba21a439f057b96ebc
zAtomic <atharvsriv99@gmail.com>
2024-12-26 18:00:45 +0530

    Introduced  Quiz Categories
    Introduced the ability for the user to choose any available quiz categories

--- QuizTimeV2/CheckIntent.py
+++ QuizTimeV2/CheckIntent.py
@@ -2,7 +2,15 @@ def start_quiz():
     print("Quiz")
     BeginA = "START\n(Yes/No)"
     print(BeginA)
-    
+
+    return input()
+
+quizCategories = ("General Knowledge", "Goofy Knowledge Check") 
+
+def choose_category():
+    print("Choose a category:")
+    for i in range(len(quizCategories)):
+        print(f"{i+1}. {quizCategories[i]}")
     return input()
 
 def check_progress(counter, qr, qrr, total_questions):
--- QuizTimeV2/QuizTimePlayer.py
+++ QuizTimeV2/QuizTimePlayer.py
@@ -1,16 +1,35 @@
 #quiz game
-from CheckIntent import start_quiz, check_progress
-from QuizTuples import questions, answers
+from CheckIntent import start_quiz, check_progress, choose_category
+from GoofyTuple import questions as goofy_questions
+from GoofyTuple import answers as goofy_answers
+from GKTuple import questions as gk_questions
+from GKTuple import questions as gk_answers
 
 while True:
+
     score = 0
     counter = 0
-    qr = (questions.__len__())//2
-    qrr = ((questions.__len__())*8)//10
+    questions = []
+    answers = []
 
-    Begin = start_quiz()
+    chosenCat = choose_category()
     
-    if Begin.lower() == "yes":
+    if chosenCat == "1":
+        questions = gk_questions
+        answers = gk_answers
+    elif chosenCat == "2":
+        questions = goofy_questions
+        answers = goofy_answers 
+    else :
+        questions = goofy_questions
+        answers = goofy_answers
+    
+    halfQuestionMilestone = (questions.__len__())//2
+    eightyPctQuestionMilestone = ((questions.__len__())*8)//10
+
+    begin = start_quiz()
+
+    if begin.lower() == "yes":
         message = ""
         isLastQuestion = False
 
@@ -24,22 +43,22 @@ while True:
             if isLastQuestion:
                 print(message)
             else:
-                message = check_progress(counter, qr, qrr, questions.__len__())
+                message = check_progress(counter, halfQuestionMilestone, eightyPctQuestionMilestone, questions.__len__())
                 if message != "":
                     print(message)
 
             print(question)
 
-            Answer = input()
-            if Answer.lower() == answers[counter]:
+            answer = input()
+            if answer.lower() == answers[counter]:
                 print("Good Job!")
                 score += 1
             else:
                 print("Wrong")
             counter +=1
 
-        End = f"Your score: {score}"
-        print(End)
+        end = f"Your score: {score}"
+        print(end)
     else :
         print("ok")
 
