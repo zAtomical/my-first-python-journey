38a7e47288bae16f0fa363bde5500826e1c46329
zAtomic <atharvsriv99@gmail.com>
2024-12-26 18:11:32 +0530

    Cleaned up the code

--- QuizTimeV2/CheckIntent.py
+++ QuizTimeV2/CheckIntent.py
@@ -1,26 +1,34 @@
+quizCategories = (
+    "General Knowledge",
+    "Goofy Knowledge Check"
+) 
+
 def start_quiz():
     print("Quiz")
-    BeginA = "START\n(Yes/No)"
-    print(BeginA)
+    startQuiz = "START\n(Yes/No)"
+    print(startQuiz)
 
-    return input()
+    startQuizOption = input()
 
-quizCategories = ("General Knowledge", "Goofy Knowledge Check") 
+    return startQuizOption
 
 def choose_category():
     print("Choose a category:")
     for i in range(len(quizCategories)):
         print(f"{i+1}. {quizCategories[i]}")
-    return input()
 
-def check_progress(counter, qr, qrr, total_questions):
+    chosenCategory = input()
+
+    return chosenCategory
+
+def check_progress(counter, halfQuestionMilestone, eightyPctQuestionMilestone, totalQuestions):
     message = ""
 
-    if counter == qr:
-        message = "Halfway there!"
-    elif counter == qrr:
+    if counter == halfQuestionMilestone:
+        message = "Halfway there!"    
+    elif counter == eightyPctQuestionMilestone:
         message = "Almost there!"
-    elif counter == total_questions:
+    elif counter == totalQuestions:
         message = "Quiz is over!"
-
+    
     return message
